124c024ebd487ea7b002519cc15a94d92813219f
mukul <mukul.jaiswal@antiersolutions.com>
2025-07-30 18:18:34 +0530

    Saving local changes before pull

--- tasks.py
+++ tasks.py
@@ -1,10 +1,12 @@
 from utils import CredentialManager
 from sierra_tasks import Tasks
+from locust import between
+
 
 class SierraDimensionsTasks:
     def __init__(self, parent):
         self.user = parent
-
+        self.wait_time = between(1, 3)  # Random wait between 1-3 seconds
 
     # ------------------------------
     # Registration Flow
@@ -14,6 +16,8 @@ class SierraDimensionsTasks:
 
         # Signup user 
         signupsuccess, user_data =Tasks.signup(self.user)
+        self.user.wait()  # Add random delay
+
 
         if not signupsuccess:
             print(f"User {self.user.user_id} signup failed, skipping further steps")
@@ -21,6 +25,7 @@ class SierraDimensionsTasks:
         
         # Verify OTP after signup
         otp_success=Tasks.verify_otp(self.user,user_data["email"],"SIGNUP")
+        self.user.wait()  # Add random delay
     
         #save_credential 
         if otp_success:
@@ -41,7 +46,7 @@ class SierraDimensionsTasks:
 
         # Login user
         loginsuccess, email = Tasks.login(self.user)
-
+        self.user.wait()  # Add random delay
 
         if not loginsuccess:
             print(f"[Worker {self.user.environment.runner.worker_index}] User {self.user.user_id}: Login failed")
@@ -49,39 +54,48 @@ class SierraDimensionsTasks:
 
         # Verify OTP after login
         otp_success = Tasks.verify_otp(self.user, email, "LOGIN")
+        self.user.wait()  # Add random delay
         if not otp_success:
             print(f"[Worker {self.user.environment.runner.worker_index}] User {self.user.user_id}: OTP verification failed")
             return
 
         # Get User Profile
         Tasks.get_user_profile(self.user)
+        self.user.wait()  # Add random delay
 
 
         # User Add Nominee List After Login.......................
         Tasks.add_nominee(self.user)
+        self.user.wait()  # Add random delay
 
         # User Nominee List After Login..................
         nominee_id=Tasks.get_nominee_list(self.user)
         print(f"User {self.user.user_id} Nominee ID: {nominee_id}") 
+        self.user.wait()  # Add random delay
 
         #Update User Nominee List After Login..................
         Tasks.update_nominee(self.user,nominee_id)
-
+        self.user.wait()  # Add random delay
 
         # Delete User Nominee List After Login..................
         Tasks.delete_nominee(self.user,nominee_id)
+        self.user.wait()  # Add random delay
 
         # #Get Profile Details After Login..................
         Tasks.get_profile_details(self.user)
+        self.user.wait()  # Add random delay
 
         #Get profile avatars
         Tasks.get_avatars(self.user)
+        self.user.wait()  # Add random delay
 
         #Upload profile-picture  (Avatar)
         Tasks.upload_avatar(self.user)
+        self.user.wait()  # Add random delay
 
         #Upload profile-picture
         Tasks.upload_profile_picture(self.user)
+        self.user.wait()  # Add random delay
 
         #Delete profile-picture
         Tasks.delete_profile_picture(self.user)
